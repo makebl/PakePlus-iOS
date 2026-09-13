cdfb321d6e70c7abec8ba6eadacdf6df5012cee5
马可 <78850005+makebl@users.noreply.github.com>
2026-09-13 19:37:54 +0800

    fix: 去掉启动图 - ppconfig.json launchImage=false + ContentView.swift 移除启动图逻辑

--- PakePlus/ContentView.swift
+++ PakePlus/ContentView.swift
@@ -13,35 +13,18 @@ struct ContentView: View {
     let webUrl = Bundle.main.object(forInfoDictionaryKey: "WEBURL") as? String ?? ""
     let debug = Bundle.main.object(forInfoDictionaryKey: "DEBUG") as? Bool ?? false
     let fullScreen = Bundle.main.object(forInfoDictionaryKey: "FULLSCREEN") as? Bool ?? false
-    let launchImage = Bundle.main.object(forInfoDictionaryKey: "LAUNCHIMAGE") as? Bool ?? false
     let screenOn = Bundle.main.object(forInfoDictionaryKey: "SCREENON") as? Bool ?? false
 
     @Environment(\.scenePhase) private var scenePhase
-    @State private var isWebLoaded: Bool = false
 
     var body: some View {
-        // BottomMenuView()
-        ZStack {
-            // webview
-            WebView(
-                webUrl: URL(string: webUrl)!,
-                debug: debug,
-                onLoadFinished: {
-                    isWebLoaded = true
-                }
-            )
-            .ignoresSafeArea(edges: [.all])
-            .allowsHitTesting(isWebLoaded)
-            
-            // loading screen
-            if !isWebLoaded && launchImage {
-                Image("LaunchScreen")
-                    .resizable()
-                    .scaledToFill()
-                    .ignoresSafeArea()
-                    .transition(.opacity)
-            }
-        }
+        // webview
+        WebView(
+            webUrl: URL(string: webUrl)!,
+            debug: debug,
+            onLoadFinished: nil
+        )
+        .ignoresSafeArea(edges: [.all])
         .statusBarHidden(fullScreen)
         .onAppear {
             UIApplication.shared.isIdleTimerDisabled = screenOn
